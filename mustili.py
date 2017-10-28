a20cac2ce96e099f0f2380a545209988c981db15
Chris D <cheesecakecgcontact@gmail.com>
2017-10-28 19:35:33 -0400

    First coommmiiiitt

---
+++ mustili.py
@@ -0,0 +1,38 @@
+import discord
+import asyncio
+import os
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    has_followed = False
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('Hello'):
+       await client.send_message(message.channel, 'What\'s up!')
+    if message.content.startswith('It\'s alive!'):
+       await client.send_message(message.channel, 'What?')
+       await client.send_message(message.channel, 'I\'m not alive...')
+    if message.content.startswith('Whos here?'):
+       print( discord.get_all_messages() )
+       await discord.send_message(message.channel, client.get_all_messages())
+    if message.content.startswith('Follow me'):# and has_followed == False:
+       msg = await client.wait_for_message(author=message.author)
+       await client.accept_invite(msg.content)
+       print( msg.content )
+       await client.send_message(message.channel, 'Now Following.')
+       has_followed = True
+       return
+    if message.content.startswith('Otters be gone!'):
+       await client.send_message(message.channel, 'Later! OwO')
+       quit()
+
+       
+client.run('MzY3ODE1MjQwMzI4NDEzMTg0.DMA6tw.pV1-lbe9Hz3xU_YdtyVLde9j5kE')

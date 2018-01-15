d757bfdf1b1b09491e9468993c55cc3c9628b512
Chris D (CheesecakeCG) <cheesecakecgcontact@gmail.com>
2018-01-14 20:51:49 -0500

    Restructured code, added one genuine soul, and borked the token security. Whose going to hack him anyway?

--- mustili.py
+++ mustili.py
@@ -1,36 +1,50 @@
 import discord
 import asyncio
 import os
+import json
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
-sync def load_client_id():
-    f = open("clientid","r")
-    return f.read()
-
+soul = json.load(open('soul.json'))
 
 client = discord.Client()
 
+def create_reply(m):
+	output = "My database does not contain a reply for that. Sorry!"
+	choices = []
+	for c in soul["responses"]:
+		if fuzz.ratio(c[0], m) > 50:
+			choices.append(c)
+
+	ext = process.extractOne(m, choices)
+	if ext[1] > 80:
+		print( "Choices: ", choices)
+		print( "ext: ", ext )
+		output = ext[0][1]
+	
+	return output
+	
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-    has_followed = False
+	print("Logged in as")
+	print(client.user.name)
+	print(client.user.id)
+	print("------")
+	has_followed = False
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('Hello'):
-       await client.send_message(message.channel, 'What\'s up!')
-    if message.content.startswith('It\'s alive!'):
-       await client.send_message(message.channel, 'What?')
-       await client.send_message(message.channel, 'I\'m not alive...')
-    if message.content.startswith('Whos here?'):
-       print( discord.get_all_messages() )
-       await discord.send_message(message.channel, client.get_all_messages())
+	print( message.content )
+	if message.content.startswith("<@367815240328413184>"):
+		await client.send_message(message.channel, create_reply(message.content.replace("<@367815240328413184>", "")))
+	else:
+		if message.content.startswith("Otters be gone!"):
+			await client.send_message(message.channel, "Later! OwO")
+			quit()
+	   
 
-    if message.content.startswith('Otters be gone!'):
-       await client.send_message(message.channel, 'Later! OwO')
-       quit()
+#f = open("clientid","r")
+#print(f.read())
 
 
-client.run(load_client_id())
+client.run("MzY3ODE1MjQwMzI4NDEzMTg0.DT1vNw.n2WlPClI5GbjRAbgC5-ydr4SOcA")

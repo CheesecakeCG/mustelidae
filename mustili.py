7af5fde8e7fcac166a051b11b8d9c6341948bde9
Chris D (CheesecakeCG) <cheesecakecgcontact@gmail.com>
2018-04-14 18:52:59 -0400

    Improved message selection

--- mustili.py
+++ mustili.py
@@ -5,24 +5,31 @@ import json
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-soul = json.load(open('cfg/soul.json'))
+soul = json.load(open("cfg/soul.json"))
 clientid = json.load(open('cfg/clientid.json'))
 
 client = discord.Client()
 
 def create_reply(m):
-	output = "My database does not contain a reply for that. Sorry!"
-	choices = []
-	for c in soul["responses"]:
-		if fuzz.ratio(c[0], m) > 50:
-			choices.append(c)
+	if not m.strip() == "":
+		output = "My database does not contain a reply for that. Sorry!"
+		choices = []
+		for c in soul["responses"]:
+			if fuzz.ratio(c[0], m) > 80:
+				choices.append(c)
 
-	ext = process.extractOne(m, choices)
-	if ext[1] > 80:
-		print( "Choices: ", choices)
-		print( "ext: ", ext )
-		output = ext[0][1]
-	
+		if len(choices) > 0:
+			ext = process.extractOne(m, choices)
+			print(len(choices) -1 / len(choices))
+			if ext[1] > min(len(choices) -1 / len(choices), 90):
+				print( "Choices: ", choices)
+				print( "ext: ", ext )
+				output = ext[0][1]
+			else:
+				pass
+	else:
+		output = "..."
+		
 	return output
 	
 @client.event
@@ -31,13 +38,15 @@ async def on_ready():
 	print(client.user.name)
 	print(client.user.id)
 	print("------")
-	has_followed = False
+	
 
 @client.event
 async def on_message(message):
-	print( message.content )
+	print( message.author, "said \"", message.content, "\"" )
 	if message.content.startswith("<@367815240328413184>"):
 		await client.send_message(message.channel, create_reply(message.content.replace("<@367815240328413184>", "")))
+	elif message.content.startswith("<!@367815240328413184>"):
+		await client.send_message(message.channel, create_reply(message.content.replace("<!@367815240328413184>", "")))
 	else:
 		if message.content.startswith("Otters be gone!"):
 			await client.send_message(message.channel, "Later! OwO")
